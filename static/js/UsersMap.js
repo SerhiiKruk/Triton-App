fabe802e7264e36941fb846ca59ede9d00a89db2
SerhiiKruk <liverpoolcout1712@gmail.com>
2020-09-28 21:38:22 +0300

    Upd

---
+++ static/js/UsersMap.js
@@ -0,0 +1,28 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamltbXk4OTgiLCJhIjoiY2tmOW51ZDVkMGg2dzJyb2Y2Y2lrenp2NCJ9.suxwFu99fm3uh2F-EdNcxw';
+
+var map;
+
+function initMap()
+{
+map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [30, 50], // starting position [lng, lat]
+zoom: 1 // starting zoom
+});
+}
+
+function showUser(longitude, latitude)
+{
+
+var lng = parseFloat(longitude)
+var lat = parseFloat(latitude)
+
+var ll = new mapboxgl.LngLat(lng,lat);
+var marker = new mapboxgl.Marker()
+.setLngLat(ll)
+.addTo(map);
+
+}
+
+
