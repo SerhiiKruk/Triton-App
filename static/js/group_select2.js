4f86b054bef2f745c583837723098222423e1483
SerhiiKruk <70840528+SerhiiKruk@users.noreply.github.com>
2020-10-15 20:45:09 +0300

    Update group_select2.js

--- static/js/group_select2.js
+++ static/js/group_select2.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
         $('#id_group_id').select2({
             theme: "classic",
             ajax: {
-                url: 'http://pyhedgehog.pythonanywhere.com/student/create/',
+                url: 'http://hedgehog94.pythonanywhere.com/student/create/',
                 delay: 500,
                 dataType: 'json',
                 processResults: function (data) {
